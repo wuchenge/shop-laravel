3cca7eba4c1968852035d71379b22309d7b41ec5
无尘阁 <ligui0506@126.com>
2018-10-22 13:55:49 +0800

    后台众筹商品管理

--- database/admin.sql
+++ database/admin.sql
@@ -21,7 +21,7 @@
 
 LOCK TABLES `admin_menu` WRITE;
 /*!40000 ALTER TABLE `admin_menu` DISABLE KEYS */;
-INSERT INTO `admin_menu` VALUES (1,0,1,'首页','fa-bar-chart','/',NULL,'2018-05-31 23:30:13'),(2,0,7,'系统管理','fa-tasks',NULL,NULL,'2018-10-19 06:57:17'),(3,2,8,'管理员','fa-users','auth/users',NULL,'2018-10-19 06:57:17'),(4,2,9,'角色','fa-user','auth/roles',NULL,'2018-10-19 06:57:17'),(5,2,10,'权限','fa-ban','auth/permissions',NULL,'2018-10-19 06:57:17'),(6,2,11,'菜单','fa-bars','auth/menu',NULL,'2018-10-19 06:57:17'),(7,2,12,'操作日志','fa-history','auth/logs',NULL,'2018-10-19 06:57:17'),(8,0,2,'用户管理','fa-users','/users','2018-05-31 23:54:14','2018-05-31 23:55:10'),(9,0,4,'商品管理','fa-cubes','/products','2018-06-01 00:28:19','2018-10-19 06:57:17'),(10,0,5,'订单管理','fa-rmb','/orders','2018-06-05 02:45:54','2018-10-19 06:57:17'),(11,0,6,'优惠券管理','fa-tags','/coupon_codes','2018-06-05 07:33:54','2018-10-19 06:57:17'),(12,0,3,'类目管理','fa-bars','/categories','2018-10-19 06:57:03','2018-10-19 06:57:17');
+INSERT INTO `admin_menu` VALUES (1,0,1,'首页','fa-bar-chart','/',NULL,'2018-05-31 23:30:13'),(2,0,9,'系统管理','fa-tasks',NULL,NULL,'2018-10-22 05:46:44'),(3,2,10,'管理员','fa-users','auth/users',NULL,'2018-10-22 05:46:44'),(4,2,11,'角色','fa-user','auth/roles',NULL,'2018-10-22 05:46:44'),(5,2,12,'权限','fa-ban','auth/permissions',NULL,'2018-10-22 05:46:44'),(6,2,13,'菜单','fa-bars','auth/menu',NULL,'2018-10-22 05:46:44'),(7,2,14,'操作日志','fa-history','auth/logs',NULL,'2018-10-22 05:46:44'),(8,0,2,'用户管理','fa-users','/users','2018-05-31 23:54:14','2018-05-31 23:55:10'),(9,0,4,'商品管理','fa-cubes','/products','2018-06-01 00:28:19','2018-10-19 06:57:17'),(10,0,7,'订单管理','fa-rmb','/orders','2018-06-05 02:45:54','2018-10-22 05:46:44'),(11,0,8,'优惠券管理','fa-tags','/coupon_codes','2018-06-05 07:33:54','2018-10-22 05:46:44'),(12,0,3,'类目管理','fa-bars','/categories','2018-10-19 06:57:03','2018-10-19 06:57:17'),(13,9,5,'众筹商品','fa-flag-checkered','/crowdfunding_products','2018-10-22 05:46:04','2018-10-22 05:46:44'),(14,9,6,'普通商品','fa-cubes','/products','2018-10-22 05:46:33','2018-10-22 05:46:44');
 /*!40000 ALTER TABLE `admin_menu` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -103,4 +103,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2018-10-19 15:01:28
+-- Dump completed on 2018-10-22 13:53:10
