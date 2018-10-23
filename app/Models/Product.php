66eea13da71959076e1c679b43f6a517638ef451
无尘阁 <ligui0506@126.com>
2018-10-23 16:09:41 +0800

    商品添加长标题字段

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -15,7 +15,7 @@ class Product extends Model
         self::TYPE_CROWDFUNDING => '众筹商品',
     ];
 
-    protected $fillable = ['title', 'description', 'image', 'on_sale', 'rating', 'sold_count', 'review_count', 'price', 'type'];
+    protected $fillable = ['title', 'description', 'image', 'on_sale', 'rating', 'sold_count', 'review_count', 'price', 'long_title', 'type'];
     protected $casts = [
         'on_sale' => 'boolean', // on_sale 是一个布尔类型的字段
     ];
